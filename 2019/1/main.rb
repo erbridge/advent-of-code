9df3373570f4bc5a0aa74d74bedf522053428428
erbridge <f@erbridge.co.uk>
2019-12-09 22:24:40 +0000

    Solve 2019, day one, part two

--- 2019/1/main.rb
+++ 2019/1/main.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
-module_masses = IO.readlines('./input.txt')
-
-fuel_by_module = module_masses.map do |mass|
-  (mass.to_i / 3) - 2 unless mass.empty?
+def fuel_needed_for(mass)
+  (mass.to_i / 3) - 2
 end
 
-total_fuel = fuel_by_module.sum
+def fuel_needed_for_modules_and(fuel_mass)
+  fuel = fuel_needed_for(fuel_mass)
+
+  return fuel_mass unless fuel.positive?
+
+  fuel_mass + fuel_needed_for_modules_and(fuel)
+end
+
+module_masses = IO.readlines('./input.txt').compact.map(&:to_i)
+
+fuel_needed_for_modules = module_masses.map { |mass| fuel_needed_for(mass) }
+fuel_needed_for_modules_and_fuel = fuel_needed_for_modules.map { |fuel_mass| fuel_needed_for_modules_and(fuel_mass) }
+
+total_fuel = fuel_needed_for_modules_and_fuel.sum
 
 puts total_fuel
