8d96b9e2f68d5a730c842ddbea709180dbc7e17b
erbridge <f@erbridge.co.uk>
2019-12-21 15:32:43 +0000

    Solve 2019, day four, part two

--- 2019/4/main.rb
+++ 2019/4/main.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
+require 'set'
+
 # rules:
 # - 6-digits
 # - inside input range
 # - at least 2 adjacent digits are equal
 # - ltr, digits never decrease
+# - there are no more than 2 digits in any sequence of identical digits
 
 input_range_values = IO.readlines('./input.txt', '-').map do |value|
   value.delete_suffix('-').to_i
@@ -13,20 +16,26 @@ end
 input_range = input_range_values.first..input_range_values.last
 
 passwords = input_range.select do |password|
-  some_digits_match = false
+  matching_digits = Set.new
+  last_last_digit = 0
   last_digit = 0
 
   non_decreasing = password.to_s.split('').all? do |digit|
-    some_digits_match = true if digit == last_digit
+    matching_digits.add(digit) if digit == last_digit && digit != last_last_digit
+
+    if digit == last_digit && digit == last_last_digit
+      matching_digits.delete(digit)
+    end
 
     next false if last_digit.to_i > digit.to_i
 
+    last_last_digit = last_digit
     last_digit = digit
 
     true
   end
 
-  some_digits_match && non_decreasing
+  non_decreasing && matching_digits.any?
 end
 
 puts passwords.count
